5e36148dc6a98d7e8db30da3efb8a14e247d09da
Weiran (Rebecca) Cheng <91348196+weiranc@users.noreply.github.com>
2022-03-03 16:05:28 -0800

    define controller method

---
+++ server/controller.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const Reactor = require('../db/Reactor');
+
+const controller = {
+  getAll: function (req, res) {
+    Reactor.find({})
+      .then((response) => {
+        res.status(200).send(response);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  },
+  getAllByFilter: function (req, res) {
+    Reactor.find({ isFoaming: req.params.isFoaming })
+      .then((response) => {
+        res.status(200).send(response);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  },
+  updateFoamingStatus: function (req, res) {
+    Reactor.findOneAndUpdate(
+      { _id: req.params.id },
+      { isFoaming: req.body.isFoaming }
+    )
+      .then((response) => {
+        res.status(204).send(response);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  },
+};
+
+module.exports = controller;
